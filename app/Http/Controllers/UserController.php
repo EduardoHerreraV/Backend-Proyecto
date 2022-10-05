e7567f52ef1bd799dc7ae5a680c2673f6442e1d9
Eduardo Herrera <pedro.herrera@becarios.infotec.mx>
2022-10-04 22:38:14 -0500

    create user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -55,6 +56,7 @@ class UserController extends Controller
             DB::beginTransaction();
             $user = new User();
             $user->fill($request->all());
+            $user->password = Hash::make($request->input('password'));
             $user->save();
             DB::commit();
             return response()->json([
