48a70564f85fcd84c777be9223e01fd1cc8bfca7
Eduardo Herrera <pedro.herrera@becarios.infotec.mx>
2022-10-13 23:51:33 -0500

    changes catalogs controller

--- app/Http/Controllers/API/CatalogsController.php
+++ app/Http/Controllers/API/CatalogsController.php
@@ -3,15 +3,8 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\Project;
-use App\Models\Task;
-use App\Models\CatExperience;
-use App\Models\CatPhase;
-use App\Models\CatSize;
-use App\Models\CatStatus;
-use App\Models\CatSpecificKnowledge;
-use App\Models\CatKnowledgeAreaType;
 use App\Models\Degree;
+use App\Models\Groups;
 
 class CatalogsController extends Controller
 {
@@ -21,32 +14,11 @@ class CatalogsController extends Controller
         try {
             $catalogs = [];
 
-            if($request->has('project') && $request->input('project') == true) {
-                $catalogs['project'] = Project::select(['id', 'name'])->get();
-            }
-            if($request->has('tasks') && $request->input('tasks') == true) {
-                $catalogs['tasks'] = Task::select(['id', 'name'])->get();
-            }
-            if($request->has('cat_knowledge_area_types') && $request->input('cat_knowledge_area_types') == true) {
-                $catalogs['cat_knowledge_area_types'] = CatKnowledgeAreaType::select(['id', 'name'])->get();
-            }
-            if($request->has('cat_specific_knowledge') && $request->input('cat_specific_knowledge') == true) {
-                $catalogs['cat_specific_knowledge'] = CatSpecificKnowledge::select(['id', 'name', 'cat_knowledge_area_types_id'])->get();
-            }
-            if($request->has('cat_experiences') && $request->input('cat_experiences') == true) {
-                $catalogs['cat_experiences'] = CatExperience::select(['id', 'name'])->get();
-            }
-            if($request->has('cat_phases') && $request->input('cat_phases') == true) {
-                $catalogs['cat_phases'] = CatPhase::select(['id', 'name'])->get();
-            }
-            if($request->has('cat_sizes') && $request->input('cat_sizes') == true) {
-                $catalogs['cat_sizes'] = CatSize::select(['id', 'name'])->get();
-            }
-            if($request->has('cat_statuses') && $request->input('cat_statuses') == true) {
-                $catalogs['cat_statuses'] = CatStatus::select(['id', 'name'])->get();
-            }
             if($request->has('degrees') && $request->input('degrees') == true) {
                 $catalogs['degrees'] = Degree::select(['id', 'name'])->get();
+            }
+            if($request->has('groups') && $request->input('groups') == true) {
+                $catalogs['groups'] = Groups::select(['id', 'name'])->get();
             }
 
             return response()->json([
